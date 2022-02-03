85f55d4614b86d2bc544f288ae92c48f653d4373
SumonRayy <sumonrayy@gmail.com>
2022-02-03 09:49:15 +0530

    🐕‍🦺 Version : added

--- translator.js
+++ translator.js
@@ -1,11 +1,12 @@
 var translateAPIKey = process.env.TRANSLATE_API_KEY;
 var translateServiceURL = "https://api.kr-seo.language-translator.watson.cloud.ibm.com";
+var translateServiceVersion = process.env.TRANSLATE_SERVICE_VERSION;
 
 const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
 const languageTranslator = new LanguageTranslatorV3({
-    version: '2018-05-01',
+    version: translateServiceVersion,
     authenticator: new IamAuthenticator({
         apikey: translateAPIKey,
     }),
